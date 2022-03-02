73e7c7cb045cc140c9d880592f29817d3e23043a
Marcin Gruda <marcingruda1@gmail.com>
2022-03-02 10:04:02 +0100

    dodanie do usera generatedvalue i inne

--- src/main/java/com/example/HomeUser/model/User.java
+++ src/main/java/com/example/HomeUser/model/User.java
@@ -16,6 +16,7 @@ import java.util.Collection;
 public class User {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String name;
