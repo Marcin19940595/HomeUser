2ea55d39a578e716c589c1c6ca592936567863d1
Marcin Gruda <marcingruda1@gmail.com>
2022-03-03 10:46:02 +0100

    generaedvalue z usera

--- src/main/java/com/example/HomeUser/model/User.java
+++ src/main/java/com/example/HomeUser/model/User.java
@@ -17,6 +17,7 @@ import java.util.Collection;
 public class User {
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String name;
