893841f3cdb8f98eb7e4675e3009aac62e2f901e
Marcin Gruda <marcingruda1@gmail.com>
2022-03-28 13:42:06 +0200

    cofniecie zmiana w controllerze i w repo findbyid

--- src/main/java/com/example/HomeUser/service/QuestionService.java
+++ src/main/java/com/example/HomeUser/service/QuestionService.java
@@ -5,7 +5,6 @@ import com.example.HomeUser.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Id;
 import java.util.List;
 import java.util.Random;
 
@@ -27,6 +26,6 @@ public class QuestionService {
     public Question drawQuestions() {
         Random random = new Random(getAllQuestion().size());
         int draw = random.nextInt(getAllQuestion().size());
-        return questionRepository.findById();
+        return getAllQuestion().get(draw);
     }
 }
