e8db242f67a647ef44cab8d8155d60d04b33d3e1
Marcin Gruda <marcingruda1@gmail.com>
2022-04-14 09:35:58 +0200

    Dodanie cross 13 linijka LoginApi

--- src/main/java/com/example/HomeUser/config/LoginApi.java
+++ src/main/java/com/example/HomeUser/config/LoginApi.java
@@ -4,14 +4,16 @@ import com.example.HomeUser.model.User;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-
+@CrossOrigin
 @RestController
 public class LoginApi {
+
     @PostMapping("/login")
     public String login (@RequestBody User user){
         long currentTimeMillis = System.currentTimeMillis();
